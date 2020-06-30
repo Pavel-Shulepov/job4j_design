eb1b9e2af2acf16aba4f5b8e3534494a6aa6b11b
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-06-30 18:12:45 +0500

    Итератор для двухмерного массива [#316311]

---
+++ src/main/java/ru/job4j/MatrixIt.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (row < data.length) && (column < data[row].length || row != data.length - 1);
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        if (column >= data[row].length) {
+            row++;
+            if (data[row].length == 0) return next();
+            column = 0;
+        }
+        return data[row][column++];
+    }
+}
