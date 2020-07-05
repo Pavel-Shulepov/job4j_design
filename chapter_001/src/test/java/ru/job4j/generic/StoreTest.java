9ca74b88769902b868fcfd228f080c07f6eae2a1
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-07-05 19:59:02 +0500

    MemStore fix checkIndex

--- chapter_001/src/test/java/ru/job4j/generic/StoreTest.java
+++ chapter_001/src/test/java/ru/job4j/generic/StoreTest.java
@@ -16,7 +16,6 @@ public class StoreTest {
         userStore.add(new User("3", "User_3"));
         userStore.add(new User("4", "User_4"));
         userStore.add(new User("5", "User_5"));
-        userStore.add(new User("3", "User_3_1"));
         assertThat(userStore.findById("3").getName(), is("User_3"));
         userStore.replace("4", new User("6", "User_6"));
         assertThat(userStore.findById("4"), is(IsNull.nullValue()));
@@ -24,7 +23,8 @@ public class StoreTest {
         userStore.delete("2");
         assertThat(userStore.findById("2"), is(IsNull.nullValue()));
         userStore.replace("1", new User("1", "User_1_1"));
-        assertThat(userStore.findById("1").getName(), is("User_1"));
+        assertThat(userStore.findById("1").getName(), is("User_1_1"));
+        userStore.add(new User("3", "User_3_1"));
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -35,7 +35,6 @@ public class StoreTest {
         roleStore.add(new Role("3", "Role_3"));
         roleStore.add(new Role("4", "Role_4"));
         roleStore.add(new Role("5", "Role_5"));
-        roleStore.add(new Role("3", "Role_3_1"));
         assertThat(roleStore.findById("3").getName(), is("Role_3"));
         roleStore.replace("4", new Role("6", "Role_6"));
         assertThat(roleStore.findById("4"), is(IsNull.nullValue()));
@@ -43,7 +42,8 @@ public class StoreTest {
         roleStore.delete("2");
         assertThat(roleStore.findById("2"), is(IsNull.nullValue()));
         roleStore.replace("1", new Role("1", "Role_1_1"));
-        assertThat(roleStore.findById("1").getName(), is("Role_1"));
+        assertThat(roleStore.findById("1").getName(), is("Role_1_1"));
+        roleStore.add(new Role("3", "Role_3_1"));
     }
 
 }
