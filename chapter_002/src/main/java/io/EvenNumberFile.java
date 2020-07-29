55a802b4b631bda03ec47b016800496739062dbc
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-07-29 13:37:40 +0500

    EvenNumberFile fix

--- chapter_002/src/main/java/io/EvenNumberFile.java
+++ chapter_002/src/main/java/io/EvenNumberFile.java
@@ -10,14 +10,21 @@ public class EvenNumberFile {
 
     public static void readFile(String fileName) {
         try (FileInputStream in = new FileInputStream(fileName)) {
-            printResult(in.readAllBytes());
+
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            printResult(text.toString());
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private static void printResult(byte[] buff) {
-        String[] lines = new String(buff).split(System.lineSeparator());
+    private static void printResult(String text) {
+        String[] lines = text.split(System.lineSeparator());
         for (String line : lines) {
             line = isEven(Integer.parseInt(line)) ? line + " - четное " : line;
             System.out.println(line);
