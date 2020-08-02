df750db3efa8d051c56bf04008b3eaf0dc90e274
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-08-02 09:04:51 +0500

    AnalizyTest with temp folder

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -2,7 +2,9 @@ package ru.job4j.io;
 
 import io.Analizy;
 import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,12 +15,16 @@ import static org.junit.Assert.assertThat;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
     public void whenWriteLogThenRead() throws IOException {
         String logPath = "./../server.log";
         List<String> unavailableList = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
-        Analizy.unavailable(logPath, "unavailable.csv");
-        assertThat(FileUtils.readLines(new File("unavailable.csv")), is(unavailableList));
+        File unFile = folder.newFile("unavailable.csv");
+        Analizy.unavailable(logPath, unFile.getAbsolutePath());
+        assertThat(FileUtils.readLines(unFile), is(unavailableList));
     }
 
 }
