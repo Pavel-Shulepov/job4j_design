c2bdd9b65fdcce8604beafa8bcaabcb4274075f4
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-08-02 18:17:17 +0500

    File

---
+++ chapter_002/src/main/java/io/Dir.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.File;
+import java.util.Objects;
+
+public class Dir {
+
+    public static void main(String[] args) {
+        File file = new File("c:\\Windows");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.printf("size : %s%n", file.getTotalSpace());
+        for (File subfile : Objects.requireNonNull(file.listFiles())) {
+            System.out.printf("%s %d %n", subfile.getName(), subfile.length());
+        }
+    }
+
+}
