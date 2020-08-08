4a58291bfee329761a3380dbd207544da7a4dd56
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2020-08-08 18:59:12 +0500

    Slf4j - вывод переменных.

--- chapter_002/src/main/java/io/UsageLog4j.java
+++ chapter_002/src/main/java/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Pavel Shulepov";
+        short age = 36;
+        int height = 164;
+        char initial = 'P';
+        double d = 100.101D;
+        long l = 123456789L;
+        float f = 1.2F;
+        byte b = 28;
+        boolean bool = true;
+        LOG.debug("User info name : {}, age : {}, height: {}, initial: {}", name, age, height, initial);
+        LOG.debug("double : {}, long : {}, float: {}, byte: {}, boolean: {}", d, l, f, b, bool);
     }
 }
