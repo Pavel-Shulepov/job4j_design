d748cbcf0aad4da2a1ffff412b4dd37de71fa5e8
Pavel Shulepov <pavel.shulepov@coderlink.cc>
2021-01-21 17:59:46 +0500

    Режим ожидания. [#316557]

---
+++ multithreading/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread wget = new Thread(
+                () -> {
+                    for (int i = 0; i < 100; i++) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + i + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        wget.start();
+    }
+}
